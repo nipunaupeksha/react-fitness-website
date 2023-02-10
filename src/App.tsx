019c38879e9def7c9a24c1c51f7e2de17cd725a4
nipunaupeksha <nipuna.upeksha@gmail.com>
2023-02-11 00:36:38 +0530

    finalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import Navbar from '@/scenes/navbar';
+import Home from "@/scenes/home";
+import OurClasses from "@/scenes/ourClasses";
+import Benefits from "@/scenes/benefits";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
 import { useState, useEffect } from 'react';
 import { SelectedPage } from '@/shared/types';
 
@@ -23,6 +28,11 @@ function App() {
   return (
     <div className="app bg-gray-20">
       <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   )
 }
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -5,3 +5,16 @@ export enum SelectedPage {
     OurClasses = "ourclasses",
     ContactUs = "contactus"
   }
+
+  export interface BenefitType {
+    icon: JSX.Element;
+    title: string;
+    description: string;
+  }
+  
+  export interface ClassType {
+    name: string;
+    description?: string;
+    image: string;
+  }
+  
